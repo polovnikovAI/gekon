f2d26be33949e56abac2ba3bce5a10abf4ceb77b
polovnikov.ai <head080203@gmail.com>
2023-11-12 16:08:12 +0500

    completed module carousel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import About from './components/About/About';
+import Carousel from './components/Carousel/Carousel';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
         <div className='App'>
             <Navbar />
             <About />
+            <Carousel />
         </div>
         
     );
---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import cl from './Carousel.module.css';
+import './Carousel.css';
+
+const Carousel = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+                }
+            }
+            ]
+        };
+
+    return (
+        <div className={cl.carousel}>
+            <div className={cl.sliderTitle}>Собственное производство</div>
+            <div className={cl.unusualPhoto}>
+                <img src={require('../../photos/production/BKL_0873.jpg')} alt='Upload error' className={cl.unusualPhoto} />
+                <img src={require('../../photos/production/BKL_0861.jpg')} alt='Upload error' className={cl.unusualPhoto} />
+            </div>
+            <Slider {...settings}>
+                <img src={require('../../photos/production/BKL_9167.jpg')} alt='Upload error' className={cl.photo} />
+                <img src={require('../../photos/production/BKL_9126.jpg')} alt='Upload error' className={cl.photo} />
+                <img src={require('../../photos/restorationDiatone2017/BKL_4930.jpg')} alt='Upload error' className={cl.photo} />
+                <img src={require('../../photos/TLM12015/TLM1/1600/BKL_9612.jpg')} alt='Upload error' className={cl.photo} />
+            </Slider>
+        </div>
+    );
+};
+
+export default Carousel;
