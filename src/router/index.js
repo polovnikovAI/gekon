5d57aebfca33e0456e6055b5cd066ab1aa1fdd61
pipistrellla <xexexexaxaxa22848@gmail.com>
2023-12-05 14:16:12 +0500

    добавил роутинг в зависимости от авторизации

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,18 @@ import Home from "../pages/Home";
 import Gallery from "../pages/Gallery";
 import Profile from "../pages/Profile"
 
-export const publicRoutes = [
+export const privateRoutes = [
     {path: '*', component: Home, exact: true},
     {path: '/gallery', component: Gallery, exact: true},
     {path: '/profile' , component: Profile, exact: true},
+]
+
+
+
+
+export const publicRoutes = [
+
+    {path: '*', component: Home, exact: true},
+    {path: '/gallery', component: Gallery, exact: true},
+    
 ]
