aadf7ea4754d5b9357c0120d092b114406ea5a23
polovnikov.ai <head080203@gmail.com>
2023-12-05 17:28:48 +0500

    completed module about for mobile devices

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import cl from './About.module.css';
+import classNames from 'classnames';
 
 const About = () => {
     return (
         <div className={cl.about} id="moduleAbout">
             <div className={cl.container}>
-                <div className={cl.about__header}>
-                    О НАС
+                <div className={cl.aboutHeader}>
+                    О нас
                 </div>
-                <div className={cl.about__text}>
-                    <div className={`${cl.about__text} ${cl.block__1}`}>
+                <div className={cl.aboutText}>
+                    <div className={classNames(cl.textCard, cl.card1)}>
                         Gekon - это компания, которая занимается производством и реставрацией акустической аппаратуры с 2015 года.
                     </div>
-                    <div className={`${cl.about__text} ${cl.block__2}`}>
-                        GEKON - это про качество
+                    <div className={classNames(cl.textCard, cl.card2)}>
+                        GEKON - это про качество.
                     </div>
-                    <div className={`${cl.about__text} ${cl.block__3}`}>
+                    <div className={classNames(cl.textCard, cl.card3)}>
                             В нашей команде работают опытные специалисты, которые знают все тонкости производства акустической аппаратуры. Мы используем только качественные материалы и современное оборудование, чтобы гарантировать высокое качество нашей продукции.
                     </div>
                 </div>
